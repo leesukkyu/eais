49ff72881fc4a5d996a72f1712c8c84b51946b63
이석규 <ansua86@gmail.com>
2020-04-08 02:50:50 +0900

    excel download

--- client/src/public/CONFIG.js
+++ client/src/public/CONFIG.js
@@ -1,14 +1,9 @@
 export const SEARCH_MONTH = 6;
-export const SIDO_CODE_URL = "http://openapi.nsdi.go.kr/nsdi/eios/service/rest/AdmService/admCodeList.json";
-export const SIDO_CODE_KEY = "";
-export const SIGOON_CODE_URL = "http://openapi.nsdi.go.kr/nsdi/eios/service/rest/AdmService/admSiList.json";
-export const SIGOON_CODE_KEY = "";
-export const DONG_CODE_URL = "http://openapi.nsdi.go.kr/nsdi/eios/service/rest/AdmService/admDongList.json";
-export const DONG_CODE_KEY = "";
+export const SIDO_CODE_URL = 'http://openapi.nsdi.go.kr/nsdi/eios/service/rest/AdmService/admCodeList.json';
+export const SIDO_CODE_KEY = '12685d425f1af0872d756c';
+export const SIGOON_CODE_URL = 'http://openapi.nsdi.go.kr/nsdi/eios/service/rest/AdmService/admSiList.json';
+export const SIGOON_CODE_KEY = 'b0888bae39fbd0463a9252';
+export const DONG_CODE_URL = 'http://openapi.nsdi.go.kr/nsdi/eios/service/rest/AdmService/admDongList.json';
+export const DONG_CODE_KEY = '91afccaa8d7f499151ee3b';
 
-export const SEARCH_KEY = "";
-
-const SERVER_URL = process.env.NODE_ENV == "development" ? "http://localhost:5001" : "";
-
-export const SEARCH_URL = `${SERVER_URL}/api/search`;
-export const COLLECTION_SEARCH_URL = `${SERVER_URL}/api/collection`;
+export const SERVER_URL = process.env.NODE_ENV == 'development' ? 'http://localhost:5500' : '';
